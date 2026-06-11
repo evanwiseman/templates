139db96323468f2d6602fe39a29277f5ac7cd54b
Evan Wiseman <evanwiseman2000@gmail.com>
2026-06-11 15:44:41 -0700

    updated_at updates automatically

--- fastapi-app/project_name/app/features/users/models.py
+++ fastapi-app/project_name/app/features/users/models.py
@@ -21,4 +21,7 @@ class User(Base):
     username: Mapped[str] = mapped_column(unique=True, index=True)
     password_hash: Mapped[str] = mapped_column()
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
