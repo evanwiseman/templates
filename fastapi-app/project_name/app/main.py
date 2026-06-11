4f037101ed7b5c0c2e5989d63576b1ca860f039c
Evan Wiseman <evanwiseman2000@gmail.com>
2026-06-11 15:59:42 -0700

    dispose of engine on shutdown

--- fastapi-app/project_name/app/main.py
+++ fastapi-app/project_name/app/main.py
@@ -12,6 +12,7 @@ from fastapi_pagination import add_pagination
 
 # First party
 from project_name.app.core.config import settings
+from project_name.app.database.engine import engine
 from project_name.app.features.users import users_router
 
 logger = logging.getLogger(__name__)
@@ -21,6 +22,7 @@ logger.setLevel(settings.log_level)
 @asynccontextmanager
 async def lifespan(_app: FastAPI) -> AsyncGenerator[None]:
     yield
+    engine.dispose()
 
 
 app = FastAPI(lifespan=lifespan)
